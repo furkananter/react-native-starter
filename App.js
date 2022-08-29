4c97b6b5e3ebe9c831e95cc42f8c77c1137cf790
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-08-29 10:20:00 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar} from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ScrollView style={styles.ScrollView}>
+        <Text style={styles.paragraph}>
+          This is a simple React Native app.
+        </Text>
+        <Text style={styles.paragraph}>
+          It is a simple React Native app.
+        </Text>
+      </ScrollView>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +21,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
+    color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  paragraph: {
+    margin: 100,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+  ScrollView: {
+    width: '100%',
   },
 });
